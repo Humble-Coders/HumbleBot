fee22ffe8dfb09b177c6353029cfecd1647d7557
Humble-Coders <humblecoders2024@gmail.com>
2025-10-31 00:44:54 +0530

    Update BASE_URL in NetworkClient to remove path

--- app/src/main/java/com/humblecoders/humblebot/api/NetworkClient.kt
+++ app/src/main/java/com/humblecoders/humblebot/api/NetworkClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkClient {
-    private const val BASE_URL = "https://generativelanguage.googleapis.com/v1beta/models/"
+    private const val BASE_URL = "https://generativelanguage.googleapis.com"
 
     private val okHttpClient = OkHttpClient.Builder()
         .build()
